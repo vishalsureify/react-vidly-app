d439d62c2c24509877625e11dad9b39dc6ef6931
Vishal Polley <vishal@sureify.com>
2019-07-08 17:24:19 +0530

    Composite Components

---
+++ src/components/common/like.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+// Input: liked: boolean
+// Output: OnCick
+
+const Like = props => {
+  let classes = "fa fa-heart";
+  if (!props.liked) classes += "-o";
+  return (
+    <i
+      onClick={props.onClick}
+      style={{ cursor: "pointer" }}
+      className={classes}
+      aria-hidden="true"
+    />
+  );
+};
+
+export default Like;
